701460c1ce314fd88d35a5246b5ace50647586b0
matiaslascano <matias_lascano@hotmail.com>
2023-07-14 17:30:35 -0300

    segundo commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,13 +21,13 @@ const Hero = () => {
           mt-10"
           handleClick={handleScroll}
         />
+      </div>
 
-        <div className="hero__image-container">
-          <div className="hero__image">
-            <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          </div>
-          <div className="hero__image-overlay"></div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   );
