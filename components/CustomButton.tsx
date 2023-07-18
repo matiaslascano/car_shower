8c53ae099949ebad94bb00b241b950df9afc4e3f
matiaslascano <matias_lascano@hotmail.com>
2023-07-18 13:03:23 -0300

    version casi final

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,7 @@
 "use client";
 
+import { CustomButtonProps } from "@/types";
 import Image from "next/image";
-
-import { CustomButtonProps } from "@types";
 
 const Button = ({
   isDisabled,
--- types/index.ts
+++ types/index.ts
@@ -7,7 +7,7 @@ export interface CustomButtonProps {
   btnType?: "button" | "submit";
   textStyles?: string;
   rightIcon?: string;
-  isDisabeld?: boolean;
+  isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
